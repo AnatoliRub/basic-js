7c85c74f534dc91bd4848130b25cc5edb489c65f
Anatoli Rubankou <catok1992@gmail.com>
2020-10-12 03:27:45 +0300

    feat: add solution for 'extended repeater' task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+
+  if(options['repeatTimes'] == undefined){
+    options['repeatTimes'] = 1;
+  }
+  if(options['addition'] === undefined){
+    options['addition'] = '';
+  }
+  if(options['addition'] == null){
+    options['addition'] = 'null';
+  }
+  if(options['additionRepeatTimes'] == undefined){
+    options['additionRepeatTimes'] = 1;
+  }
+
+  let answer =''
+
+  for (let i = 0; i < options['repeatTimes']; i++) {
+    answer = answer + str
+      for (let j = 0; j < options['additionRepeatTimes']; j++) {
+        j != (options['additionRepeatTimes'] - 1) ? answer = answer + (options['addition'].toString() || '') + (options['additionSeparator'] || '') :  answer = answer + (options['addition'].toString() || '')
+      }
+      i != (options['repeatTimes'] - 1) ? answer = answer + (options['separator'] || '+') : answer
+  }
+
+  return answer
 };
-  
