bbdcd4f4f3b05f43e9a03538b84c1823d3c20dad
Anatoli Rubankou <catok1992@gmail.com>
2021-03-09 01:14:14 +0300

    fix: solving of task "carbon-dating js"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  return dateSample.arguments.length === 0 || typeof sampleActivity !== String
+  console.log(typeof sampleActivity);
+  console.log(parseFloat(sampleActivity));
+  let answer = (dateSample.arguments.length === 0) || ((typeof sampleActivity).toString().toUpperCase() !== "String".toUpperCase()) || parseFloat(sampleActivity) > 8000 || parseFloat(sampleActivity) < 1 || isNaN(parseFloat(sampleActivity))
   ?
   false
   :
-  (Math.log( MODERN_ACTIVITY / parseFloat(sampleActivity))  / ( 0.693 / HALF_LIFE_PERIOD ) / 100.0) * 100.0
+  Math.ceil((Math.log( MODERN_ACTIVITY / parseFloat(sampleActivity))  / ( 0.693 / HALF_LIFE_PERIOD ) / 100.0) * 100.0)
+  return answer < 0 ? false : answer;
 };
